ee5d8ae6b6efb420d65cccc67a45fc1a06e39e87
Rodrigo Ortega Cuesta <rodortcue@gmail.com>
2016-01-23 21:08:32 +0100

    Add testing

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+// Reference: http://karma-runner.github.io/0.13/config/configuration-file.html
+
+require('babel-register');
+
+module.exports = function karmaConf(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    reporters: ['spec', 'coverage'],
+    files: [
+      'node_modules/phantomjs-polyfill/bind-polyfill.js',
+      'src/**/*.spec.*',
+    ],
+    preprocessors: {
+      'src/**/*.spec.*': ['webpack', 'sourcemap']
+    },
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    coverageReporter: {
+      dir: 'build/coverage/',
+      type: 'html',
+    },
+    webpack: require('./webpack.config.babel'),
+    webpackMiddleware: { noInfo: true },
+  });
+}
---
+++ src/DumbCounter/DumbCounter.spec.js
@@ -0,0 +1,6 @@
+describe('DumbCounter', function() {
+  it('should do boilerplate things', function() {
+    // TODO: test something now
+    expect(true).to.equal(true);
+  });
+});
